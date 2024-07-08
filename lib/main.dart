ca84eb92e35f672bf87cfdc6961f9aa3fbe2de78
Your Name <sidratulpunno>
2024-07-08 12:55:52 +0600

    Added categories file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:meals/categories.dart';
+
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
@@ -22,7 +24,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: theme,
-        home: // Todo ...,
+        home: const Categories(),
     );
   }
 }

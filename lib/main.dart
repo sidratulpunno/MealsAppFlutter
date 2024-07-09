9bc1f6a4f082dbb4b1012e8b3363ef3c0c0d290b
Your Name <sidratulpunno>
2024-07-09 13:01:17 +0600

    Added meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/screens/categories.dart';
+import 'package:meals/data/dummy_data.dart';
+
 
 final theme = ThemeData(
   useMaterial3: true,
